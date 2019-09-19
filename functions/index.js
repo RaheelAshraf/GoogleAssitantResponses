4de45c4547ad12101adf9bbdbb4d74871f17c99f
iamraheelashraf <labkoat@gmail.com>
2019-09-19 09:13:56 +0500

    second commit

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
+'use strict';
+
+const {
+  dialogflow,
+  SimpleResponse,
+  Suggestions
+} = require('actions-on-google');
+
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+const app = dialogflow({ debug: true });
+
+app.intent('Default Welcome Intent', (conv) => {
+  conv.ask(new SimpleResponse({
+    speech: `Here's an example of a simple response. ` +
+      `Which type of response would you like to see next?`,
+    text: `Here's a simple response. ` +
+      `Which response would you like to see next?`,
+  }));
 });
+
+app.intent('SSML', (conv) => {
+  conv.ask(`<speak>` +
+    `Here are <say-as interpet-as="characters">SSML</say-as> examples.` +
+    `Here is a buzzing fly ` +
+    `<audio src="https://actions.google.com/sounds/v1/animals/buzzing_fly.ogg"></audio>` +
+    `and here's a short pause <break time="800ms"/>` +
+    `</speak>`);
+  conv.ask('Which response would you like to see next?');
+});
+
+exports.dialogflowWebhook = functions.https.onRequest(app);
